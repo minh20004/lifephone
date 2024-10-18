60e7b4e9f41fea7867c4b1770eaf23bd7d12bb0e
Pham  Quang Minh <Minhtit2004vkc@gmail.com>
2024-10-18 16:19:22 +0700

    quang minh

--- resources/views/admin/layout/partials/appMenu.blade.php
+++ resources/views/admin/layout/partials/appMenu.blade.php
@@ -37,7 +37,7 @@
                 <div class="collapse menu-dropdown" id="sidebarDashboards">
                     <ul class="nav nav-sm flex-column">
                         <li class="nav-item">
-                            <a href="{{ route('user.home') }}" class="nav-link" data-key="t-analytics"> Xem trang </a>
+                            <a href="{{ route('home') }}" class="nav-link" data-key="t-analytics"> Xem trang </a>
                         </li>
                         <li class="nav-item">
                             <a href="dashboard-crm.html" class="nav-link" data-key="t-crm"> CRM </a>
